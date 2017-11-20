d449e0bedb3586b69433bb8132be7f961d3fdaf5
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2017-11-20 10:04:37 -0600

    Starting 1st example but having erros
    error : unbound prefix

---
+++ Andriod Studio Examples/EX 1/app/src/main/java/com/example1/xarichar/firstexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example1.xarichar.firstexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
